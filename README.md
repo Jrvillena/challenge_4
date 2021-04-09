67942152a801bc068c2091ab228067eb80f228b6
justin <justinrvillena@gmail.com>
2021-04-09 11:30:11 -0700

    layout made

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ test to push to guthub<Br>
 create a basic html file to start with<br>
 added header<br>
 created style css to correspond with assignment<br>
+created layout and pseudo codes<br>
---
+++ assets/sctript.js
@@ -0,0 +1,34 @@
+// Set variables
+
+var startQuiz = document.getElementById("#start");
+
+var el = document.querySelector("#start");
+if(el){
+    console.log("its working");
+  };
+
+
+
+// Function startQuiz 
+    // Timer starts
+
+
+    // Quiz questions pop up
+
+// Answering right or wrong
+
+    // Take off 15 seconds of time
+
+//If timer runs out OR questions finished time stops
+
+//Call to action quizStop
+
+    // Prompt to put in initials
+
+    // Calculate score
+
+// Put score into leaderboard 'localstorage'
+
+
+
+// Call to action 'Start Quiz' by click of button
