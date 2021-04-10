4bb9d2f19f73adb4929eb96ac11cb03ee85f1120
justin <justinrvillena@gmail.com>
2021-04-09 20:55:03 -0700

    header and start button created

--- assets/questions.js
+++ assets/questions.js
@@ -1,1 +1,44 @@
+// Store quiz questions:
 
+var questionArr = [
+    {
+    question: "what part is not an internal component of a combustion engine?",
+    choiceA : "air filter",
+    choiceB : "piston head",
+    choiceC : "cam shaft",
+    choiceD : "valve springs",
+    correct : "A"
+    },  
+    {
+    question: "what is not a standard form of drive train?",
+    choiceA : "front wheel drive",
+    choiceB : "rear wheel drive",
+    choiceC : "side wheel drive",
+    choiceD : "all wheel drive",
+    correct : "C"
+    }, 
+    {
+    question: "what oil is standard for honda?",
+    choiceA : "5w30",
+    choiceB : "10w20",
+    choiceC : "20w40",
+    choiceD : "15w20",
+    correct : "A"
+    },
+    {
+    question: "what does BMW stand for?",
+    choiceA : "big meaty whales",
+    choiceB : "Bayerische Motoren Werke Aktiengesellschaft",
+    choiceC : "been mellow world",
+    choiceD : "bug means war",
+    correct : "B"
+    }, 
+    {
+    question: "the most american made vehicle is?",
+    choiceA : "chevrolet silverado",
+    choiceB : "toyota camry",
+    choiceC : "honda civic",
+    choiceD : "BMW 3 series",
+    correct : "B"
+    }
+];
